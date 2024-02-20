c407764d74b83b6571da6208bb1366e3c60f04ce
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2024-02-20 17:16:25 +0300

    dds

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -8,10 +7,11 @@ public class Main {
     private static final List<Integer> nums;
 
     static {
-        nums = new ArrayList<>(List.of(1, 1, 2 , 2 , 3, 4, 4, 5, 6 ));
+        nums = new ArrayList<>(List.of(1, 1, 2, 2, 3, 4, 4, 5, 6));
     }
 
-    private static final List <String> words;
+    private static final List<String> words;
+
     static {
         words = new ArrayList<>(List.of("qwe", "ewq", "ewq", "weq", "ert"));
     }
@@ -25,12 +25,37 @@ public class Main {
 
     public static void task1() {
         for (int num : nums) {
-            if (num % 2 == 1){
+            if (num % 2 == 1) {
                 System.out.println(num + "");
             }
         }
+    }
+
+
+    public static void task2() {
+        Collections.sort(nums);
+        int num1 = Integer.MIN_VALUE;
+
+        for (Integer num : nums) {
+            if (num % 2 == 0 & num != num1) {
+                num1 = num;
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static void task3() {
+        Set<String> words1 = new HashSet<>(words);
+        System.out.println(words1);
+    }
+
+    public static void task4() {
+        Set<String> words1 = new HashSet<>(words);
+        System.out.println(words.size() - words1.size());
+    }
+
+
 
 
     }
 
-}
